b4528b8ad7f1f6a23b03fb99f5c382a96273bb19
Fuuma <masa81hiro69@gmail.com>
2024-02-25 17:40:31 +0900

    Initial Commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by hiro on 2024/02/25.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
